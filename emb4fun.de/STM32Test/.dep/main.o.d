3a60bb87a40fd993c79fe9586d77328e0a57625f
Denis Aleksandrov <denal05@gmail.com>
2014-09-13 18:47:06 +0200

    Added STM32Test from emb4fun.de with instructions at http://salvatoremenendez.blogspot.com/2011/08/mac-os-eclipse-openocd-stm32-arm-cortex.html

---
+++ emb4fun.de/STM32Test/.dep/main.o.d
@@ -0,0 +1,13 @@
+src/main.o: src/main.c \
+ /opt/gcc-arm-none-eabi-4_8-2014q2/lib/gcc/arm-none-eabi/4.8.4/include/stdint.h \
+ /opt/gcc-arm-none-eabi-4_8-2014q2/arm-none-eabi/include/stdint.h \
+ /opt/gcc-arm-none-eabi-4_8-2014q2/arm-none-eabi/include/machine/_default_types.h \
+ /opt/gcc-arm-none-eabi-4_8-2014q2/arm-none-eabi/include/sys/features.h
+
+/opt/gcc-arm-none-eabi-4_8-2014q2/lib/gcc/arm-none-eabi/4.8.4/include/stdint.h:
+
+/opt/gcc-arm-none-eabi-4_8-2014q2/arm-none-eabi/include/stdint.h:
+
+/opt/gcc-arm-none-eabi-4_8-2014q2/arm-none-eabi/include/machine/_default_types.h:
+
+/opt/gcc-arm-none-eabi-4_8-2014q2/arm-none-eabi/include/sys/features.h:
---
+++ emb4fun.de/STM32Test/.dep/system_stm32f10x.o.d
@@ -0,0 +1,26 @@
+cmsis/device/system_stm32f10x.o: cmsis/device/system_stm32f10x.c \
+ cmsis/device/stm32f10x.h cmsis/core/core_cm3.h \
+ /opt/gcc-arm-none-eabi-4_8-2014q2/lib/gcc/arm-none-eabi/4.8.4/include/stdint.h \
+ /opt/gcc-arm-none-eabi-4_8-2014q2/arm-none-eabi/include/stdint.h \
+ /opt/gcc-arm-none-eabi-4_8-2014q2/arm-none-eabi/include/machine/_default_types.h \
+ /opt/gcc-arm-none-eabi-4_8-2014q2/arm-none-eabi/include/sys/features.h \
+ cmsis/core/core_cmInstr.h cmsis/core/core_cmFunc.h \
+ cmsis/device/system_stm32f10x.h
+
+cmsis/device/stm32f10x.h:
+
+cmsis/core/core_cm3.h:
+
+/opt/gcc-arm-none-eabi-4_8-2014q2/lib/gcc/arm-none-eabi/4.8.4/include/stdint.h:
+
+/opt/gcc-arm-none-eabi-4_8-2014q2/arm-none-eabi/include/stdint.h:
+
+/opt/gcc-arm-none-eabi-4_8-2014q2/arm-none-eabi/include/machine/_default_types.h:
+
+/opt/gcc-arm-none-eabi-4_8-2014q2/arm-none-eabi/include/sys/features.h:
+
+cmsis/core/core_cmInstr.h:
+
+cmsis/core/core_cmFunc.h:
+
+cmsis/device/system_stm32f10x.h:
